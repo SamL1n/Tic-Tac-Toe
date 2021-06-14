03052d9880ece08832c71116ea41226549628d35
Sam <375522026@qq.com>
2021-06-14 12:53:42 +0800

    add the game logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SquareComponent } from './component/square/square.component';
+import { BoardComponent } from './component/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SquareComponent
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
--- src/app/component/square/square.component.ts
+++ src/app/component/square/square.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css']
 })
-export class SquareComponent implements OnInit {
+export class SquareComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input() value!: "X" | "O";
+  constructor() {
+    
+   }
+  
 }
